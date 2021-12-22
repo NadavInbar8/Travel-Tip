d784c30e42183161023cca9c26d508643067c2de
Michael Aharoni <michaelaharoni001@gmail.com>
2021-12-22 17:00:49 +0200

    adding a marker

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -2,74 +2,87 @@ import { API_KEY } from '../personal.js';
 import { STORAGE } from './storage.service.js';
 
 export const mapService = {
-  initMap,
-  addMarker,
-  panTo,
+    initMap,
+    addMarker,
+    panTo,
 };
 
 var gMap;
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-  const myLatLng = { lat: lat, lng: lng };
-  console.log('InitMap');
-  return _connectGoogleApi().then(() => {
-    console.log('google available');
-    gMap = new google.maps.Map(document.querySelector('#map'), {
-      center: myLatLng,
-      zoom: 15,
-    });
+    const myLatLng = { lat: lat, lng: lng };
+    console.log('InitMap');
+    return _connectGoogleApi().then(() => {
+        console.log('google available');
+        gMap = new google.maps.Map(document.querySelector('#map'), {
+            center: myLatLng,
+            zoom: 15,
+        });
 
-    let infoWindow = new google.maps.InfoWindow({
-      content: 'click me to get lat/lng',
-      position: myLatLng,
-    });
-    infoWindow.open(gMap);
+        let infoWindow = new google.maps.InfoWindow({
+            content: 'click me to get lat/lng',
+            position: myLatLng,
+        });
+        infoWindow.open(gMap);
 
-    gMap.addListener('click', (mapsMouseEvent) => {
-      // close the current window
-      infoWindow.close();
-      // create new window
-      infoWindow = new google.maps.InfoWindow({
-        position: mapsMouseEvent.latLng,
-      });
-      infoWindow.setContent(
-        `the Lat is ${mapsMouseEvent.latLng
-          .toJSON()
-          .lat.toFixed(2)} and the Lng ${mapsMouseEvent.latLng
-          .toJSON()
-          .lng.toFixed(2)}`
-        // JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-      );
-      infoWindow.open(gMap);
+        gMap.addListener('click', (mapsMouseEvent) => {
+            // close the current window
+            infoWindow.close();
+            // create new window
+            infoWindow = new google.maps.InfoWindow({
+                position: mapsMouseEvent.latLng,
+            });
+            infoWindow.setContent(
+                `the Lat is ${mapsMouseEvent.latLng
+                    .toJSON()
+                    .lat.toFixed(2)} and the Lng ${mapsMouseEvent.latLng
+                        .toJSON()
+                        .lng.toFixed(2)}`
+                // JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+            );
+            infoWindow.open(gMap);
+        });
+        console.log('Map!', gMap);
     });
-    console.log('Map!', gMap);
-  });
 }
 
 function addMarker(loc) {
-  var marker = new google.maps.Marker({
-    position: loc,
-    map: gMap,
-    title: 'Hello World!',
-  });
-  return marker;
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!',
+    });
+    return marker;
 }
 
 function panTo(lat, lng) {
-  var laLatLng = new google.maps.LatLng(lat, lng);
-  gMap.panTo(laLatLng);
+    var laLatLng = new google.maps.LatLng(lat, lng);
+    gMap.panTo(laLatLng);
 }
 
 function _connectGoogleApi() {
-  if (window.google) return Promise.resolve();
-  var elGoogleApi = document.createElement('script');
-  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-  console.log('hello');
-  elGoogleApi.async = true;
-  document.body.append(elGoogleApi);
+    if (window.google) return Promise.resolve();
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    console.log('hello');
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
 
-  return new Promise((resolve, reject) => {
-    elGoogleApi.onload = resolve;
-    elGoogleApi.onerror = () => reject('Google script failed to load');
-  });
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load');
+    });
 }
+
+//  function askName() { 
+//     const { value: email } = Swal.fire({
+//         title: 'Input email address',
+//         input: 'email',
+//         inputLabel: 'Your email address',
+//         inputPlaceholder: 'Enter your email address'
+//       })
+      
+//       if (email) {
+//         Swal.fire(`Entered email: ${email}`)
+//       }
+// }
