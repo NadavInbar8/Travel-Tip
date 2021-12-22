4b643e5721c8b17bb09869eb1d2bb0df10d46352
NadavInbar8 <nadavfrc@gmail.com>
2021-12-22 17:12:39 +0200

    added panTo

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -4,8 +4,8 @@ export const locService = {
 };
 
 const locs = [
-  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+  { name: 'Greatplace', lat: 32.047104, lng: 34.832384, id: 10001 },
+  { name: 'Neveragain', lat: 32.047201, lng: 34.832581, id: 10002 },
 ];
 
 function getLocs() {
